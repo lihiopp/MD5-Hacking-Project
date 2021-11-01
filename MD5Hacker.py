912720cb13f32e34c1c31cc7535bb5050175cbc0
lihiopp <93098326+lihiopp@users.noreply.github.com>
2021-11-01 14:28:26 +0200

    MD5 Hacker version 2
    Corresponds to The Soosim's server.

--- MD5Hacker.py
+++ MD5Hacker.py
@@ -1,33 +1,37 @@
 import socket, threading, hashlib
-import piro
+import Piro
 
 class Patzhan:
-    def __init__(self,port):
+    def __init__(self):
         self.socket = socket.socket()
-        self.socket.bind(("127.0.0.1",port))
-        self.socket.connect(("127.0.0.1",13370))
+        self.socket.connect(("10.30.58.40",13370))
         print("Connected")
     
-    def reconnect(self,port):
+    def reconnect(self):
         self.socket.send("Howdy".encode())
-        id = self.socket.recv(1024).decode()
+        id1 = self.socket.recv(1024).decode()
+        print(id1)
         self.socket.close()
         self.socket = socket.socket()
-        self.socket.bind(("127.0.0.1",port+int(id)))
-        self.socket.listen(1)
+        self.socket.bind(("10.30.56.116",13370+int(id1)))
+        print(f"bind {13370+int(id1)}")
+        self.socket.listen()
+        print("listening")
         self.clientSoc, addr = self.socket.accept()
-        Patzhan.start_hacking(self,id)
+        print("server connected")
+        Patzhan.start_hacking(self,id1)
 
     def start_hacking(self,id):
         msg = self.clientSoc.recv(1024).decode()
+        print(msg)
         while True:                
             md5 = msg.split(',')[2]
             start = msg.split(',')[0]
             stop = msg.split(',')[1]
 
             
-            t1_stop =HandleRanges(stop,start)
-            if(t1_stop[2]=='z'):
+            t1_stop =Patzhan.HandleRanges(stop,start)
+            if(t1_stop[7]=='z'):
                 t2_start=chr(ord(t1_stop[0])+1)+"aaaaaaa"
             else:
                 t2_start =t1_stop[0:7]+chr(ord(t1_stop[7])+1)
@@ -36,8 +40,8 @@ class Patzhan:
             thread_lst = [None,None]
                 
             #starts the threads, each one with it's own range values
-            thread_lst[0] = threading.Thread(target=Patzhan.find_password, args=(start,t1_stop,md5,results_lst,0))
-            thread_lst[1] = threading.Thread(target=Patzhan.find_password, args=(t2_start,stop,md5,results_lst,1))
+            thread_lst[0] = threading.Thread(target=Patzhan.generate, args=(start,t1_stop,md5,results_lst,0))
+            thread_lst[1] = threading.Thread(target=Patzhan.generate, args=(t2_start,stop,md5,results_lst,1))
             
             thread_lst[0].start()    
             thread_lst[1].start()  
@@ -56,59 +60,46 @@ class Patzhan:
             self.clientSoc.send(answer_str.encode())
 
             msg1 = self.clientSoc.recv(1024).decode()
-            if(msg1.split(",")[0]=="FINISH"):
+            if(msg1.split(",")[0]=="finish"):
                 #finish shenanigans- lights and music
-                piro.main()
+                Piro.main()
                 msg=self.clientSoc.recv(1024).decode()
             else:
                 msg = msg1
             
-      
-    def to_ascii_list(str):
-        lst = []
-        for c in str:
-            lst.append(ord(c))
-        return lst
+    
+    def generate(start,stop,md5, results_lst,i):
+        stop_char='z'
+        length=len(start)
+        s='a'
+        ok=True
 
-    def to_str(ascii_list):
-        str = ''
-        for c in ascii_list:
-            str += chr(c)
-        return str
-
-    #this function searches for the password and returns true or false. if found it appends the password to the result list
-    def find_password(start,stop,md5,results_lst,i):
-        password = start
-        pass_ascii = []
-        find = md5
-        
-        while password != '' and password != stop:
-            print(password)
-            
-            if hashlib.md5(password.encode()).hexdigest() == find:
+        while ok:
+            if(hashlib.md5(start.encode()).hexdigest()== md5):
                 results_lst[i] = True
-                results_lst.append(Patzhan.to_str(pass_ascii)) #adds the found password to the result list
+                results_lst.append(start) #adds the found password to the result list
                 return 
+            index=1
+            if  chr(ord(start[length-index]) + 1)>stop_char: 
+                flag=True 
+                while flag:
+                    #print "in while "+start[length-index]
+                    start=start[:length-index-1]+chr(ord(start[length-index-1]) + 1)+start[length-index:]
+                    #start[length-index-1]+='1'
+                    start=start[:length-index]+ s + start[length-index+1:]
+                    if (index+1)>=length:
+                        flag=False
+                    if start[length-index-1]<=stop_char:
+                        flag=False
+                    index+=1
             
-            pass_ascii = Patzhan.to_ascii_list(password)
-
-            for n in reversed(range(len(pass_ascii))):
-                pass_ascii[n] += 1
-
-                if pass_ascii[n] > ord('z'):                    
-                    if n == 0:
-                        pass_ascii = ''
-                        break
-                
-                    pass_ascii[n] = ord('a')
-
-                else:
-                    break
-
-            password = Patzhan.to_str(pass_ascii)
-        
+            else:
+                start=start[:length-index]+chr(ord(start[length-index]) + 1)+start[length-index+1:]
+            if start>stop:
+                ok=False
         results_lst[i] = False
-        return 
+        return
+        
     
     def HandleRanges(stop,start):
         int_to_char=[ord(stop[0])+ord(start[0]),ord(stop[1])+ord(start[1]),ord(stop[2])+ord(start[2]),ord(stop[3])+ord(start[3]),ord(stop[4])+ord(start[4]),ord(stop[5])+ord(start[5]),ord(stop[6])+ord(start[6]),ord(stop[7])+ord(start[7])]
@@ -126,7 +117,8 @@ class Patzhan:
             int_to_char[i]=x
         return (chr(int_to_char[0])+chr(int_to_char[1])+chr(int_to_char[2])+chr(int_to_char[3])+chr(int_to_char[4])+chr(int_to_char[5])+chr(int_to_char[6])+chr(int_to_char[7]))## end of thread1, to get startof thread2 just add to the ascii +1
 
+
 if __name__=="__main__":
-    pz = Patzhan(12345)
+    pz = Patzhan()
     while True:
-        pz.reconnect(12345)
+        pz.reconnect()
